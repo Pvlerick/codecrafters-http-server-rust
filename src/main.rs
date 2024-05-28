95dbffe1c96f777194116a057a0094f3237896a3
Philippe Vlérick <pvlerick@gmail.com>
2024-05-28 12:02:38 +0200

    extract url path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::Read,
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    const RESP: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
+    const RESP_200: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
+    const RESP_404: &[u8] = "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes();
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -9,11 +14,24 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let _ = stream.write_all(RESP);
+                let mut buf = [0u8; 1024];
+                let read_bytes = stream.read(&mut buf).unwrap();
+                if read_bytes > 0 {
+                    let req = String::from_utf8_lossy(&buf[..read_bytes]);
+                    let data: Vec<&str> = req.split_whitespace().take(2).collect();
+                    match data[..] {
+                        ["GET", "/"] => write_response(&mut stream, RESP_200),
+                        _ => write_response(&mut stream, RESP_404),
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn write_response(mut stream: &TcpStream, response: &[u8]) {
+    let _ = stream.write_all(response);
 }
