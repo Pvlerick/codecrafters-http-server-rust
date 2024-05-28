ff5546f824a77538adf42a436dc458ecfbbe25d5
Philippe Vlérick <pvlerick@gmail.com>
2024-05-28 11:46:48 +0200

    200 response

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
+    const RESP: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let _ = stream.write_all(RESP);
             }
             Err(e) => {
                 println!("error: {}", e);
