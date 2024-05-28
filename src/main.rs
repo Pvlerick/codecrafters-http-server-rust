f7f21e6bae95d2ce0323632d0d1bf5912f0c7f28
Philippe Vlérick <pvlerick@gmail.com>
2024-05-28 15:34:29 +0200

    user-agent

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::{
+    collections::HashMap,
     io::Read,
     io::Write,
     net::{TcpListener, TcpStream},
 };
 
-fn main() {
-    const RESP_200: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
-    const RESP_404: &[u8] = "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes();
+const RESP_200: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
+const RESP_404: &[u8] = b"HTTP/1.1 404 Not Found\r\n\r\n";
 
+const ROOT_PATH: &[u8] = b"/";
+const ECHO_PATH: &[u8] = b"/echo/";
+const USERAGENT_PATH: &[u8] = b"/user-agent";
+
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
@@ -18,11 +23,15 @@ fn main() {
                 let read_bytes = stream.read(&mut buf).unwrap();
                 if read_bytes > 0 {
                     let req = HttpRequest::parse(&buf[..read_bytes]).unwrap();
-                    match (req.verb.as_ref(), &req.path.as_bytes()) {
-                        ("GET", [47]) => write_response(&mut stream, RESP_200),
+                    match (req.verb.as_ref(), req.path.as_bytes()) {
+                        ("GET", ROOT_PATH) => write_response(&mut stream, RESP_200),
                         ("GET", [47, 101, 99, 104, 111, 47, content @ ..]) => {
                             write_response(&mut stream, &HttpResponse::build("200 OK", content));
                         }
+                        ("GET", USERAGENT_PATH) => write_response(
+                            &mut stream,
+                            req.headers.get("User-Agent").unwrap().as_bytes(),
+                        ),
                         _ => write_response(&mut stream, RESP_404),
                     }
                 }
@@ -40,17 +49,29 @@ fn write_response(mut stream: &TcpStream, response: &[u8]) {
 struct HttpRequest {
     verb: String,
     path: String,
+    headers: HashMap<String, String>,
 }
 
 impl HttpRequest {
     fn parse(req: &[u8]) -> Option<HttpRequest> {
         let req = String::from_utf8_lossy(&req);
-        let req: Vec<_> = req.split_whitespace().collect();
+        let req: Vec<_> = req.split_terminator("\r\n").collect();
 
-        if req.len() > 3 {
+        let request_line = req.first().unwrap();
+        let request_line: Vec<_> = request_line.split_whitespace().collect();
+
+        let headers: HashMap<String, String, _> = HashMap::from_iter(
+            req[1..req.len() - 1]
+                .iter()
+                .map(|i| i.split(":").collect::<Vec<_>>())
+                .map(|i| (i[0].to_string(), i[1].trim().to_string())),
+        );
+
+        if request_line.len() >= 3 {
             Some(HttpRequest {
-                verb: req[0].to_owned(),
-                path: req[1].to_owned(),
+                verb: request_line[0].to_owned(),
+                path: request_line[1].to_owned(),
+                headers,
             })
         } else {
             None
@@ -72,5 +93,20 @@ impl HttpResponse {
         res.extend_from_slice(content);
 
         res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_request() {
+        let sut = HttpRequest::parse(b"GET /user-agent HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: foobar/1.2.3\r\nAccept: */*\r\n\r\n").unwrap();
+        assert_eq!("GET", sut.verb);
+        assert_eq!("/user-agent", sut.path);
+        assert_eq!("localhost", sut.headers.get("Host").unwrap());
+        assert_eq!("foobar/1.2.3", sut.headers.get("User-Agent").unwrap());
+        assert_eq!("*/*", sut.headers.get("Accept").unwrap());
     }
 }
